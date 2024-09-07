0de9ebeee1911e2f0b2795563758033015516b6f
xavelo <xavelo@gmail.com>
2024-09-07 12:07:32 +0200

    ping

--- src/main/java/com/xavelo/kafka/KafkaProducerController.java
+++ src/main/java/com/xavelo/kafka/KafkaProducerController.java
@@ -1,14 +1,9 @@
 package com.xavelo.kafka;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.info.GitProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,19 +20,11 @@ public class KafkaProducerController {
     private String podName;
 
     @Autowired
-    private GitProperties gitProperties;
-
-    @Autowired
     private KafkaService kafkaService;
 
-    @GetMapping("/hello")
-    public ResponseEntity<Hello> hello() {
-        String commitId = gitProperties.getCommitId();
-        LocalDateTime dateTime = LocalDateTime.ofInstant(gitProperties.getCommitTime(), ZoneId.systemDefault());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String commitTime = dateTime.format(formatter);
-        logger.info("hello from pod {} - commitId {} - commitTime {}", commitId, commitTime, podName);
-        return ResponseEntity.ok(new Hello("hello from pod " + podName, commitId + " - " + commitTime));
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("ping from pod " + podName);
     }
 
     @PostMapping("/produce")
